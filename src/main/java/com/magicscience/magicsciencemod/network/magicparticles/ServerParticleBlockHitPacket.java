79aec040d8ada11bceab4753b3dccec7d8d7042e
Vitalii <vitalik.chu@mail.ru>
2025-08-21 23:29:38 +0300

    Fix merge

--- src/main/java/com/magicscience/magicsciencemod/network/magicparticles/ServerParticleBlockHitPacket.java
+++ src/main/java/com/magicscience/magicsciencemod/network/magicparticles/ServerParticleBlockHitPacket.java
@@ -1,18 +1,14 @@
 package com.magicscience.magicsciencemod.network.magicparticles;
 
-import com.magicscience.magicsciencemod.aspects.cores.CoreTypes;
+import com.magicscience.magicsciencemod.aspects.cores.CoreTypeHelper;
 import com.magicscience.magicsciencemod.network.IServerPacket;
-import com.magicscience.magicsciencemod.registry.ModNetwork;
 import com.mojang.logging.LogUtils;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.item.PrimedTnt;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.minecraftforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
@@ -63,37 +59,41 @@ public class ServerParticleBlockHitPacket implements IServerPacket {
         int coreId = additionalArgs.contains("coreId") ? additionalArgs.getInt("coreId"):-1;
 
         LOGGER.info("Block hit at {} state {} coreId {}", blockPos, state, coreId);
-        if (coreId==CoreTypes.FIRE.getId()) {
-            if (state.getBlock()==Blocks.TNT) {
-                level.removeBlock(blockPos, false);
 
-                var centerBlockPos = blockPos.getCenter();
-                PrimedTnt primed = new PrimedTnt(
-                    level,
-                    centerBlockPos.x,
-                    centerBlockPos.y,
-                    centerBlockPos.z,
-                    player
-                );
-                level.addFreshEntity(primed);
-                LOGGER.info("Ignited TNT at {}", blockPos);
-                return;
-            }
+        var core = CoreTypeHelper.findInstance(coreId);
+        core.processingBlock(state.getBlock(), level, blockHitResult, player);
 
-            // todo: particle remove
-            if (state.isFlammable(level, blockPos, blockHitResult.getDirection())) {
-                var abovePos = blockPos.relative(blockHitResult.getDirection());
-
-                if (level.getBlockState(abovePos).isAir()) {
-                    level.setBlockAndUpdate(abovePos, Blocks.FIRE.defaultBlockState());
-                    LOGGER.info("Ignited block at {} with fire on {}", abovePos, blockPos);
-
-                    ModNetwork.CHANNEL.send(
-                        PacketDistributor.TRACKING_ENTITY_AND_SELF.with(() -> player),
-                        new ClientRemoveParticlePacket(particleUUID)
-                    );
-                }
-            }
-        }
+//        if (coreId==CoreTypes.FIRE.getId()) {
+//            if (state.getBlock()==Blocks.TNT) {
+//                level.removeBlock(blockPos, false);
+//
+//                var centerBlockPos = blockPos.getCenter();
+//                PrimedTnt primed = new PrimedTnt(
+//                    level,
+//                    centerBlockPos.x,
+//                    centerBlockPos.y,
+//                    centerBlockPos.z,
+//                    player
+//                );
+//                level.addFreshEntity(primed);
+//                LOGGER.info("Ignited TNT at {}", blockPos);
+//                return;
+//            }
+//
+//            // todo: particle remove
+//            if (state.isFlammable(level, blockPos, blockHitResult.getDirection())) {
+//                var abovePos = blockPos.relative(blockHitResult.getDirection());
+//
+//                if (level.getBlockState(abovePos).isAir()) {
+//                    level.setBlockAndUpdate(abovePos, Blocks.FIRE.defaultBlockState());
+//                    LOGGER.info("Ignited block at {} with fire on {}", abovePos, blockPos);
+//
+//                    ModNetwork.CHANNEL.send(
+//                        PacketDistributor.TRACKING_ENTITY_AND_SELF.with(() -> player),
+//                        new ClientRemoveParticlePacket(particleUUID)
+//                    );
+//                }
+//            }
+//        }
     }
 }
