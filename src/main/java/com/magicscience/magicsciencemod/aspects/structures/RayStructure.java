c989dd24fb0e824430636d9640f226078b06e231
PLAPTYPUS <johndoe@example.com>
2025-09-04 21:58:57 +0300

    Start/end mathutils

--- src/main/java/com/magicscience/magicsciencemod/aspects/structures/RayStructure.java
+++ src/main/java/com/magicscience/magicsciencemod/aspects/structures/RayStructure.java
@@ -9,6 +9,10 @@ import org.jetbrains.annotations.NotNull;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RayStructure extends BaseMagicStructure {
+    public static final double RAY_LENGTH = 3.0;
+    public static final double MAX_OFFSET_X = 0.10;
+    public static final double MAX_OFFSET_Y = 0.10;
+    public static final double MAX_OFFSET_Z = 0.10;
     private static final IBaseStructureConfig CONFIG = StructureConfig.get(StructureTypes.RAY);
 
     public RayStructure(@NotNull BaseStructureData baseStructureData, int stack) {
@@ -34,20 +38,15 @@ public class RayStructure extends BaseMagicStructure {
     public Vec3 calculateStartParticlePosition(Vec3 basePosition) {
         var random = ThreadLocalRandom.current();
         var mc = Minecraft.getInstance();
-        if (mc.player==null) return basePosition;
-
-        final double rayLength = 3.0;
-        final double maxOffsetX = 0.10;
-        final double maxOffsetY = 0.10;
-        final double maxOffsetZ = 0.10;
+        if (mc.player == null) return basePosition;
 
         Vec3 dir = mc.player.getLookAngle().normalize();
 
-        double distanceAlongBeam = random.nextDouble() * rayLength * this.getSize();
+        double distanceAlongBeam = random.nextDouble() * RAY_LENGTH * this.getSize();
 
-        double xOffset = (random.nextDouble() - 0.5) * maxOffsetX;
-        double yOffset = (random.nextDouble() - 0.5) * maxOffsetY;
-        double zOffset = (random.nextDouble() - 0.5) * maxOffsetZ;
+        double xOffset = (random.nextDouble() - 0.5) * MAX_OFFSET_X;
+        double yOffset = (random.nextDouble() - 0.5) * MAX_OFFSET_Y;
+        double zOffset = (random.nextDouble() - 0.5) * MAX_OFFSET_Z;
 
         Vec3 pointOnBeam = basePosition.add(dir.scale(distanceAlongBeam));
         return pointOnBeam.add(xOffset, yOffset, zOffset);
